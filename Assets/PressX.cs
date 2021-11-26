e1de773f2e5e2010a3baabfa7ed02ea391d849fe
Zaida Kodzoeva <zkod777@gmail.com>
2021-11-26 16:14:19 +0400

    Renamed Folders, Attempt 1 at Player Clues

---
+++ Assets/PressX.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressX : MonoBehaviour
+{
+    
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+          //GetComponentInChildren<Text>().text = "Press X";
+    }
+    
+}
