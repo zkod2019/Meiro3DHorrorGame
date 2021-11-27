b745525eeefb2b977301a2a028432eac7b5033cd
Zaida Kodzoeva <zkod777@gmail.com>
2021-11-27 22:41:29 +0400

    Attempt 2 at PressX for hints

--- Assets/PressX.cs
+++ Assets/PressX.cs
@@ -1,13 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PressX : MonoBehaviour
 {
+   // public TextMesh pressX;
     
+    
+    public Text pressX;
+   /*
+   void Start()
+    {
+        //Text sets your text to say this message
+        pressX.text = "This is my text";
+    }
+
+    void Update()
+    {
+        //Press the space key to change the Text message
+        if (Input.GetKey(KeyCode.Space))
+        {
+            pressX.text = "My text has now changed.";
+        }
+    }
+   */
     void OnCollisionEnter (Collision collisionInfo)
     {
-          //GetComponentInChildren<Text>().text = "Press X";
+       //Debug.Log (collisionInfo.collider.name);
+        //pressX.text = "Press X" + collisionInfo.collider.name;
+
+
+        GetComponentInChildren<Text>().text = "Press X";
     }
+    
     
 }
