ccd33e093b9f06651bd18abd1280743226ca74ee
Zaida Kodzoeva <zkod777@gmail.com>
2021-12-28 23:41:08 +0400

    Added Timer, Added Maze Floor, Added User Input Display

--- Assets/PressX.cs
+++ Assets/PressX.cs
@@ -2,46 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
-
-public class PressX : MonoBehaviour
-{
+public class PressX : MonoBehaviour{
     public GameObject pressX;
+    public GameObject inputText;
     public GameObject hint;
 
-    void Start()
-    {
+    //public GameObject hh ; 
+
+    void Start(){
+        //hh= (GameObject)UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>("Assets/IQ");
         pressX.SetActive(false);
         hint.SetActive(false);
+        inputText.SetActive(false);
     }
 
-    void OnTriggerEnter(Collider player)
-    {
-        //pressX.SetActive(false);
-
+    void OnTriggerEnter(Collider player){
         if(player.gameObject.tag == "Player"){
-            
-        //    if (player.gameObject.GetComponent<Collider>().bounds.Contains(GetComponent<Collider>().bounds.min) 
-        //       && player.gameObject.GetComponent<Collider>().bounds.Contains(GetComponent<Collider>().bounds.max)) {
-             pressX.SetActive(true);
-
-
-            //}
+            pressX.SetActive(true);
         }
     }
 
-    void OnTriggerExit(Collider player)
-    {
+    void OnTriggerExit(Collider player){
         if(player.gameObject.tag == "Player"){
-             pressX.SetActive(false);
+            pressX.SetActive(false);
+            hint.SetActive(false);
+            inputText.SetActive(false);
         }
     }
 
     void Update(){
+        Texture2D[] results = Array.ConvertAll(Resources.LoadAll("IQ", typeof(Texture2D)), asset => (Texture2D)asset);
+        // string path = "Assets/IQ";
+	    // test = (GameObject)AssetDatabase.LoadAssetAtPath<GameObject> (path);
+
+        // for(int i = 0; i < gameobject.transform.GetChildCount(); i++)
+        // {
+        //     Transform Children = gameobject.transform.GetChild(i);
+        // }
+
+    //    for(int i = 0; i < results.si; i++)
+    //     {
+    //         Transform Children = gameobject.transform.GetChild(i);
+    //     }
+
         if (Input.GetKeyDown(KeyCode.Space) && pressX.active){
             hint.SetActive(!hint.active);
+            inputText.SetActive(!inputText.active);
         }
-
     }
 
 
