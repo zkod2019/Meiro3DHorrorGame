474b52fe5fd0745783a51aa9c9bb83228f4254a8
Zaida Kodzoeva <zkod777@gmail.com>
2021-12-31 18:51:19 +0400

    Add dynamic hint loading for barrels

---
+++ Assets/HintController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintController : MonoBehaviour {
+    void Start() {
+        // Pseudocode:
+        //   1. Load all the question image textures (and answer texts)
+        //   2. Get all the barrels in the level
+        //   3. For every barrel, assign a question (and answer)
+
+
+        // Let's try some real code now :/
+        //   1. Load all the question image textures (and answer texts)
+        Sprite[] iqQuestions = Array.ConvertAll(Resources.LoadAll("iq-questions", typeof(Sprite)), asset => (Sprite)asset);
+        TextAsset[] iqAnswers = Array.ConvertAll(Resources.LoadAll("iq-answers", typeof(TextAsset)), asset => (TextAsset)asset);
+
+        //   2. Get all the barrels in the level
+        GameObject[] barrels = GameObject.FindGameObjectsWithTag("barrel");
+
+        //   3. For every barrel, assign a question (and answer)
+        for (int i = 0; i < barrels.Length; i++) {
+            barrels[i].GetComponent<PressX>().hintSprite = iqQuestions[i];
+            barrels[i].GetComponent<PressX>().answer = iqAnswers[i].text;
+        }
+    }
+}
--- Assets/PressX.cs
+++ Assets/PressX.cs
@@ -7,82 +7,39 @@ using System;
 public class PressX : MonoBehaviour{
     public GameObject pressX;
     public GameObject inputText;
-    public GameObject hint;
+    GameObject hint;
 
-    //public GameObject hh ; 
+    public Sprite hintSprite;
+    public String answer;
 
-    void Start(){
-        //hh= (GameObject)UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>("Assets/IQ");
+    void Start() {
+        this.hint = GameObject.Find("Canvas").transform.GetChild(0).gameObject;
+        Debug.Log(this.hint);
         pressX.SetActive(false);
         hint.SetActive(false);
         inputText.SetActive(false);
     }
 
-    void OnTriggerEnter(Collider player){
+    void OnTriggerEnter(Collider player) {
         if(player.gameObject.tag == "Player"){
+            hint.GetComponent<Image>().sprite = hintSprite;
             pressX.SetActive(true);
         }
     }
 
-    void OnTriggerExit(Collider player){
+    void OnTriggerExit(Collider player) {
         if(player.gameObject.tag == "Player"){
+            hint.GetComponent<Image>().sprite = null;
             pressX.SetActive(false);
             hint.SetActive(false);
             inputText.SetActive(false);
         }
     }
 
-    void Update(){
-        Texture2D[] results = Array.ConvertAll(Resources.LoadAll("IQ", typeof(Texture2D)), asset => (Texture2D)asset);
-        // string path = "Assets/IQ";
-	    // test = (GameObject)AssetDatabase.LoadAssetAtPath<GameObject> (path);
-
-        // for(int i = 0; i < gameobject.transform.GetChildCount(); i++)
-        // {
-        //     Transform Children = gameobject.transform.GetChild(i);
-        // }
-
-    //    for(int i = 0; i < results.si; i++)
-    //     {
-    //         Transform Children = gameobject.transform.GetChild(i);
-    //     }
-
-        if (Input.GetKeyDown(KeyCode.Space) && pressX.active){
-            hint.SetActive(!hint.active);
-            inputText.SetActive(!inputText.active);
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) && pressX.activeSelf){
+            hint.SetActive(!hint.activeSelf);
+            inputText.SetActive(!inputText.activeSelf);
         }
     }
-
-
-    /*
-   // public TextMesh pressX;
-    
-    
-    public Text pressX;
-   
-   void Start()
-    {
-        //Text sets your text to say this message
-        pressX.text = "This is my text";
-    }
-
-    void Update()
-    {
-        //Press the space key to change the Text message
-        if (Input.GetKey(KeyCode.Space))
-        {
-            pressX.text = "My text has now changed.";
-        }
-    }
-   
-    void OnCollisionEnter (Collision collisionInfo)
-    {
-       Debug.Log (collisionInfo.collider.name);
-        pressX.text = "Press X" + collisionInfo.collider.name;
-
-
-        //GetComponentInChildren<Text>().text = "Press X";
-    }
-    
-    */
 }
