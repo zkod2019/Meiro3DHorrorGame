37573c50db12c2f27e886f35667808d93fbbb0e7
Zaida Kodzoeva <zkod777@gmail.com>
2022-01-24 00:48:02 +0400

    Collecting all objects opens the exit of the maze.

--- Assets/PlayerInventory.cs
+++ Assets/PlayerInventory.cs
@@ -6,13 +6,26 @@ using UnityEngine.UI;
 public class PlayerInventory : MonoBehaviour
 {
     public List<Collectable> inventory;
+    public int count;
+    public GameObject exitMaze;
 
     public void Add(Collectable obj) {
         inventory.Add(obj);
-
+        exitMaze.SetActive(true);
+        count = 0;
         GetComponentInChildren<Text>().text = "INVENTORY: ";
         foreach (Collectable collectable in inventory) {
             GetComponentInChildren<Text>().text += collectable.collectableName + ", ";
+            count++;
+
+            if (count==8){
+                Debug.Log("All Obj Collected");
+                exitMaze.SetActive(false);
+                Debug.Log("Exit is open!");
         }
+        }
+
+        
+
     }
 }
